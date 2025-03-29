07084a037070292263212362dfa77c10d867deea
potatoSalad21 <eeperpeepo@gmail.com>
2025-03-29 11:31:03 +0400

    initial commit

---
+++ WordLadder/WordLadder.cpp
@@ -0,0 +1,22 @@
+/*
+ * File: WordLadder.cpp
+ * --------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Word Ladder problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "StanfordCPPLib/console.h"
+#include "StanfordCPPLib/lexicon.h"
+using namespace std;
+
+int main() {
+    Lexicon data("EnglishWords.dat");
+    foreach (string word in data) {
+
+    }
+
+    return 0;
+}
