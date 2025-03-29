6c7be04c03657d7a28ad1af22bbe596780bb1577
potatoSalad21 <eeperpeepo@gmail.com>
2025-03-29 16:13:07 +0400

    getting user input and reading data

--- WordLadder/WordLadder.cpp
+++ WordLadder/WordLadder.cpp
@@ -8,15 +8,37 @@
  */
 
 #include <iostream>
+#include <vector>
 #include "StanfordCPPLib/console.h"
+#include "StanfordCPPLib/simpio.h"
 #include "StanfordCPPLib/lexicon.h"
-using namespace std;
+#include "StanfordCPPLib/queue.h"
+using std::string;
+
+string getLadder(Lexicon &words, string start, string dest);
 
 int main() {
     Lexicon data("EnglishWords.dat");
-    foreach (string word in data) {
+    while (true) {
+        string startWord = getLine("Enter start word (RETURN to exit): ");
+        string endWord = getLine("Enter destination word (RETURN to exit): ");
+        if (startWord == "" || endWord == "") break; // validate input
+        if (startWord.length() != endWord.length()) {
+            std::cout << "Can't find a ladder" << std::endl;
+            continue;
+        }
 
+        getLadder(data, startWord, endWord);
     }
 
     return 0;
+}
+
+/* Constructs the word ladder with start and destination words
+* return: word ladder string
+*/
+string getLadder(Lexicon &words, string start, string dest) {
+    Queue<vector<string>> ladder;
+
+    return "";
 }
