e1b47e5f95e7de72b62b68796d3af05867a6d761
potatoSalad21 <eeperpeepo@gmail.com>
2025-04-01 15:44:05 +0400

    FEAT: getting user input and reading files

--- RandomWriter/RandomWriter.cpp
+++ RandomWriter/RandomWriter.cpp
@@ -8,10 +8,42 @@
  */
 
 #include <iostream>
-#include "console.h"
-using namespace std;
+#include <fstream>
+#include "StanfordCPPLib/console.h"
+#include "StanfordCPPLib/simpio.h"
+using std::string;
+
+void getCharFrequencies(std::ifstream);
+void generateText();
 
 int main() {
-    // [TODO: fill with your code]
+    string textName = getLine("Enter the text file name: ");
+    std::ifstream file{ textName };
+    while (!file) { // validating input
+        std::cerr << "Error: Couldn't find the file" << std::endl;
+        string textName = getLine("Enter the CORRECT text file name: ");
+        file = std::ifstream{ textName };
+    }
+
+    int markovNum = -1;
+    while (markovNum < 0 || markovNum > 10) {
+        markovNum = getInteger("Set the markov model number (1-10): ");
+    }
+
+
     return 0;
+}
+
+// Function reads file by characters and constructs the matrix of character frequencies
+void getCharFrequencies(std::ifstream &data) {
+    char ch;
+    while (data.get(ch)) {
+
+    }
+
+}
+
+// Generates the text according to the chosen markov model and model training text
+void generateText() {
+
 }
