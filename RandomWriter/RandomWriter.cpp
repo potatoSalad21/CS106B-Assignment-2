819abf907431b13bc736c140fa6bee41dfad875e
potatoSalad21 <eeperpeepo@gmail.com>
2025-04-01 23:23:04 +0400

    getting that sweet sweet decomposition going

--- RandomWriter/RandomWriter.cpp
+++ RandomWriter/RandomWriter.cpp
@@ -9,33 +9,46 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <unordered_map>
 #include "StanfordCPPLib/console.h"
 #include "StanfordCPPLib/simpio.h"
-using std::string;
+using namespace std;
 
-void getCharFrequencies(std::ifstream);
+unordered_map<string, vector<char>> getCharFrequencies(ifstream);
 void generateText();
+ifstream getFile();
+int getMarkov();
 
 int main() {
     string textName = getLine("Enter the text file name: ");
-    std::ifstream file{ textName };
-    while (!file) { // validating input
-        std::cerr << "Error: Couldn't find the file" << std::endl;
-        string textName = getLine("Enter the CORRECT text file name: ");
-        file = std::ifstream{ textName };
-    }
+    ifstream file = getFile();
+    int markovNum = getMarkov();
 
+    return 0;
+}
+
+// Function gets the text file name from user and validates input
+ifstream getFile() {
+    while (true) {
+        string textName = getLine("Enter the CORRECT text file name: ");
+        ifstream file = ifstream{ textName };
+        if (file) return file;
+    }
+}
+
+// Function gets the valid markov number input
+int getMarkov() {
     int markovNum = -1;
     while (markovNum < 0 || markovNum > 10) {
         markovNum = getInteger("Set the markov model number (1-10): ");
     }
 
-
-    return 0;
+    return markovNum;
 }
 
 // Function reads file by characters and constructs the matrix of character frequencies
-void getCharFrequencies(std::ifstream &data) {
+unordered_map<string, vector<char>> getCharFrequencies(ifstream &data) {
     char ch;
     while (data.get(ch)) {
 
